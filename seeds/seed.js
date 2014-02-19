d457828d2d0d9ca34449291261c921623093307a
Lluís Ulzurrun de Asanza Sàez <sumolari@gmail.com>
2014-02-19 19:22:43 +0100

    Improved seeding mechanism to load all seeds available

--- seeds/seed.js
+++ seeds/seed.js
@@ -26,31 +26,40 @@ model.ready.then( function ()
 		var filename = path.basename( dependencies[ _d ], '.json' );
 		filename = filename.charAt( 0 ).toUpperCase() + filename.slice( 1 );
 		seeds[ filename ] = content;
+		promises[ filename ] = Q.defer();
 	}
 
-	promises[ 'Skill' ] = Q.defer();
-	// Load skills.
-	model.Skill.remove(
-	{}, function ( err )
+	var seed_model = function ( collection )
 	{
-		var local_promises = [];
-		if ( err )
+		return function ( err )
 		{
-			log.error( err, 'SKILL SEEDING' );
-		}
-		else
-		{
-			for ( var _i in seeds[ 'Skill' ] )
+			var local_promises = [];
+			if ( err )
 			{
-				var new_skill = new model.Skill( seeds[ 'Skill' ][ _i ] );
-				var new_skill_promise = Q.npost( new_skill, "save" );
-				local_promises.push( new_skill_promise );
+				log.error( err, collection + ' SEEDING' );
 			}
-		}
-		Q.all( local_promises ).then( promises[ 'Skill' ].resolve ).fail( promises[ 'Skill' ].reject );
-	} );
+			else
+			{
+				for ( var _i in seeds[ collection ] )
+				{
+					var new_item = new model[ collection ]( seeds[ collection ][ _i ] );
+					var new_item_promise = Q.npost( new_item, "save" );
+					local_promises.push( new_item_promise );
+				}
+			}
+			Q.all( local_promises ).then( promises[ collection ].resolve ).fail( promises[ collection ].reject );
+		};
+	};
 
-	Q.all( [ promises[ 'Skill' ].promise ] ).then( function ()
+	for ( var collection in seeds )
+		model[ collection ].remove(
+		{}, seed_model( collection ) );
+
+	var plain_promises = [];
+	for ( var p in promises )
+		plain_promises.push( promises[ p ].promise );
+
+	Q.all( plain_promises ).then( function ()
 	{
 		log.success( 'Database seeded successfully', 'SEED' );
 		process.exit( 0 );
