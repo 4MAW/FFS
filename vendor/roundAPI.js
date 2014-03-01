2ca250729d081da122484fe303081e2deb2d50fe
Lluís Ulzurrun de Asanza Sàez <sumolari@gmail.com>
2014-03-01 11:29:24 +0100

    Added first version of roundAPI.

--- vendor/roundAPI.js
+++ vendor/roundAPI.js
@@ -25,22 +25,25 @@ module.exports = {
 	 */
 
 	/**
-	 * Performs given callback in this round at given phase.
+	 * Performs given callback in this round.
 	 * @param  {Function} callback Callback to run.
-	 * @param  {string}   phase    Round phase when given callback should be run.
+	 * @param  {Object}   tthis    Object to be used as caller of callback.
 	 * @return {Promise}           Promise about performing given action.
 	 */
-	"do": function ( callback, phase ) {
-
+	"do": function ( callback, tthis )
+	{
+		// @TODO This should be recored in an array of actions performed this round so it can be animated by the clients.
+		callback.apply( tthis );
 	},
 	/**
 	 * Performs given callback N rounds after current round.
 	 * @param  {[type]}   rounds   Amount of rounds that will pass before running the callback, including current round.
 	 * @param  {Function} callback Callback to run.
+	 * @param  {Object}   tthis    Object to be used as caller of callback.
 	 * @param  {string}   phase    Round phase when given callback should be run.
 	 * @return {string}            UUID of registered callback.
 	 */
-	"in": function ( rounds, callback, phase )
+	"in": function ( rounds, callback, tthis, phase )
 	{
 		if ( callbacks_once[ rounds ] === undefined )
 			callbacks_once[ rounds ] = {};
@@ -48,7 +51,16 @@ module.exports = {
 			callbacks_once[ rounds ][ phase ] = {};
 		var uuid = crypt.nonce();
 		var defer = Q.defer();
-		callbacks_once[ rounds ][ phase ][ uuid ] = defer.promise.then( callback );
+		// @TODO This should be recored in an array of actions performed this round so it can be animated by the clients.
+		// Probably we would use something like:
+		// callbacks_once[ rounds ][ phase ][ uuid ] = defer.promise.then( ··· callback ··· ).then( log_action ).fail( log_action_failed );
+		for ( var i = 0; i < rounds; i++ )
+			if ( callbacks_once[ i ] === undefined )
+				callbacks_once[ i ] = {};
+		callbacks_once[ rounds ][ phase ][ uuid ] = defer.promise.then( function ()
+		{
+			callback.apply( tthis );
+		} );
 		callback_defers[ uuid ] = defer;
 		return uuid;
 	},
@@ -58,20 +70,24 @@ module.exports = {
 	 */
 	"cancel": function ( callback_uuid )
 	{
-		callbacks_defers[ callback_uuid ].reject();
+		callback_defers[ callback_uuid ].reject();
 	},
 	/**
 	 * Registers a callback so it is performed every round at given phase.
 	 * @param  {Function} callback Callback to run.
+	 * @param  {Object}   tthis    Object to be used as caller of callback.
 	 * @param  {string}   phase    Round phase when given callback should be run.
 	 * @return {string}            UUID of callback, to be used as identifier to unregister the callback later.
 	 */
-	"each": function ( callback, phase )
+	"each": function ( callback, tthis, phase )
 	{
 		if ( callbacks_each[ phase ] === undefined )
 			callbacks_each[ phase ] = {};
 		var uuid = crypt.nonce();
-		callbacks_each[ phase ][ uuid ] = callback;
+		callbacks_each[ phase ][ uuid ] = {
+			callback: callback,
+			_this: tthis
+		};
 		return uuid;
 	},
 	/**
@@ -94,11 +110,15 @@ module.exports = {
 
 	/**
 	 * Finishes current round, removing elements from array and increasing counter.
+	 * @return {Object} Object with the actions performed by each player, indicating the order and the results.
 	 */
 	"finishRound": function ()
 	{
 		callbacks_once.shift();
 		current_round++;
+
+		// @TODO This method should return the results of this round so they can be sent to players.
+		//return everything_done_and_the_results;
 	},
 	/**
 	 * Performs callbacks of given phase for current round.
@@ -111,21 +131,29 @@ module.exports = {
 		// We want to wait until all callbacks have been executed.
 		var promises = [];
 
-		for ( var uuid in callbacks_once[ 0 ][ phase ] )
+		// If there are callbacks added to this round and this phase...
+		if ( callbacks_once[ 0 ] !== undefined && callbacks_once[ 0 ][  phase ] !== undefined )
 		{
-			// This defer will be resolved when the callback has been executed.
-			var defer = Q.defer();
-			promises.push( defer.promise );
-			// Maybe a callback is not executed because it's cancelled.
-			// That's ok, we won't wait for it but we want to use Q.all
-			// and a rejected defer will break the wait, so we resolve
-			// the defer to use Q.all without any other change.
-			callbacks_once[ 0 ][ phase ].then( defer.resolve ).fail( defer.resolve );
-			// Resolve the defer so the skill is executed.
-			// If the skill was cancelled the defer would be already rejected
-			// and this will be equivalent to noop.
-			callback_defers[ uuid ].resolve();
+			for ( var uuid in callbacks_once[ 0 ][ phase ] )
+			{
+				// This defer will be resolved when the callback has been executed.
+				var defer = Q.defer();
+				promises.push( defer.promise );
+				// Maybe a callback is not executed because it's cancelled.
+				// That's ok, we won't wait for it but we want to use Q.all
+				// and a rejected defer will break the wait, so we resolve
+				// the defer to use Q.all without any other change.
+				callbacks_once[ 0 ][ phase ][ uuid ].then( defer.resolve ).fail( defer.resolve );
+				// Resolve the defer so the skill is executed.
+				// If the skill was cancelled the defer would be already rejected
+				// and this will be equivalent to noop.
+				callback_defers[ uuid ].resolve();
+			}
 		}
+
+		if ( callbacks_each[ phase ] !== undefined )
+			for ( var i in callbacks_each[ phase ] )
+				callbacks_each[ phase ][ i ].callback.apply( callbacks_each[ phase ][ i ]._this );
 
 		return Q.all( promises );
 	},
