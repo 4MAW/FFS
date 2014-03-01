35f3ce28904b812e07c6196eacd7753301a061ca
Lluís Ulzurrun de Asanza Sàez <sumolari@gmail.com>
2014-03-01 11:29:12 +0100

    Refactored methods in characterHelper to keep track of altered status.

--- vendor/characterHelper.js
+++ vendor/characterHelper.js
@@ -4,6 +4,10 @@ var Q = require( 'q' );
 var model = require( '../models/model.js' );
 var Constants = require( './constants.js' );
 
+// Data structures.
+
+var altered_statuses = {};
+
 // Instance methods.
 
 var hello = function ()
@@ -11,57 +15,199 @@ var hello = function ()
 	return "Hello, my name's " + this.name;
 };
 
-var skillList = function()
+/**
+ * Returns the list of skills this character can use actively.
+ * @return {Array} List of active skills of this character.
+ */
+var skillList = function ()
 {
+	// Maybe a weapon could offer a new active skill...
 	return this.class.skills;
 };
 
-var stats = function()
+/**
+ * Returns an array of final stats, including any modification from boons and equipment.
+ * @return {Object} Object where each key is an stat ID and the value is the value for that stat.
+ */
+var stats = function ()
 {
-	var returnStats = this.class.stats;
-	for(var weap in this.weapons){
-		this.class.stats[0].value+=this.weapons[weap].stats.value;
+	var returnStats = JSON.parse( JSON.stringify( this.class.stats ) ); // We don't want to alter base stats.
+
+	var stat, stat_id, stat_value; // To prevent redefining these variables in each loop.
+
+	for ( var weap in this.weapons )
+	{
+		for ( stat in this.weapons[ weap ].stats )
+		{
+			stat_id = this.weapons[ weap ].stats[ stat ].stat.id;
+			stat_value = this.weapons[ weap ].stats[ stat ].value;
+			returnStats[ stat_id ] += stat_value;
+		}
 	}
-	for(var piece in Constants.ARMOR_ELEMENTS){
-		this.class.stats[1].value+=this[Constants.ARMOR_ELEMENTS[piece]].stats.value;
+
+	for ( var piece in Constants.ARMOR_ELEMENTS )
+	{
+		for ( stat in this[ Constants.ARMOR_ELEMENTS[ piece ] ].stats )
+		{
+			stat_id = this[ Constants.ARMOR_ELEMENTS[ piece ] ].stats[ stat ].stat.id;
+			stat_value = this[ Constants.ARMOR_ELEMENTS[ piece ] ].stats[ stat ].value;
+			returnStats[ stat_id ] += stat_value;
+		}
+	}
+
+	return returnStats;
+};
+
+var alterStat = function () {};
+
+/**
+ * Returns whether this character is affected by given altered status or not.
+ * @param  {Array[string]}  statuses Array of altered status to be checked.
+ * @return {boolean}                 Whether this character is affected by ANY of given status.
+ */
+var has_status = function ( statuses )
+{
+	for ( var s in statuses )
+		if ( altered_statuses[ statuses[ s ] ] !== undefined )
+			return true;
+	return false;
+};
+
+/**
+ * Returns whether this character is affected by given altered status or not.
+ * @param  {Array[string]}  statuses Array of altered status to be checked.
+ * @return {boolean}                 Whether this character is affected by ALL given status.
+ */
+var has_all_status = function ( statuses )
+{
+	var affected = true;
+	for ( var s in statuses )
+		affected = affected && ( altered_statuses[ statuses[ s ] ] !== undefined );
+	return affected;
+};
+
+
+/**
+ * Sets altered status if priority is bigger than current reason's priority.
+ * @param {Array[string]}   statuses Array of altered status to set.
+ * @param {SkillDefinition} skill    Definition of skill that is setting these statuses.
+ * @param {integer|boolean} priority Priority of new skill affecting given status.
+ *                                   The bigger the more priority it has. A value of true means maximuim priority.
+ * @return {Array[boolean]}          Whether each status was affected by given skill or not.
+ */
+var set_status = function ( statuses, skill, priority )
+{
+	var affected = [];
+
+	for ( var s in statuses )
+	{
+		var status = statuses[ s ];
+		// If status was not altered or the priority is lower than the new one's...
+		if ( altered_statuses[ status ] === undefined || altered_statuses[ status ].priority <= priority )
+		{
+			altered_statuses[ status ] = {
+				skill: skill,
+				priority: priority
+			};
+			affected[ s ] = true;
+		}
+	}
+
+	return affected;
+};
+
+/**
+ * Unsets given altered status if and only if given skill skill is the one responsible for the altered status
+ * or priority is true.
+ *
+ * This also calls cancel() method of skills removed.
+ *
+ * @param  {Array[string]}   statuses Altered statuses to remove.
+ * @param  {SkillDefinition} skill    Definition os skill that is the reason to remove the statuses.
+ * @param  {boolean}         override Whether this skill should ignore priorities or not.
+ */
+var unset_status = function ( statuses, skill, override )
+{
+	for ( var s in statuses )
+	{
+		var status = statuses[ s ];
+		// If status was not altered or the priority is lower than the new one's...
+		if ( altered_statuses[ status ] !== undefined )
+			if ( override || altered_statuses[ status ].skill === skill )
+			{
+				altered_statuses[ status ].skill.cancel( statuses );
+				delete altered_statuses[ status ];
+			}
 	}
 };
 
-var alterStat = function(){}
+/**
+ * Damages the player given the amount of damage, the type and the element of the hit.
+ * @param  {integer} amount  Base amount of health points to decrease.
+ * @param  {integer} margin  Margin where the damage will be located.
+ * @param  {string}  type    Type of damage: "physic" or "magic"
+ * @param  {string}  element Element of damage: "poison" or "".
+ */
+var damage = function ( amount, margin, type, element )
+{
+	// Compute a random damage in range amount±margin.
+	// To perform a fixed damage just pass margin=0 when calling this method.
+	var actual_damage = Math.max( amount + margin * ( Math.random() * 2 - 1 ), 0 ); // Damage can't be negative!
+	// @TODO Take into account the type of damage and the element.
+	this.class.stats[ Constants.HEALTH_STAT_ID ] -= actual_damage;
+};
 
-var newStatus = function(skillDef, round){
-	var rand = (Math.random() < 0.5) ? -1: 1;
 
-	if(this.status[skillDef.id] - round.currentRound() < skillDef.duration+rand)
-		this.status[skillDef.id] = [{skillDef.id,skillDef.duration+rand}];
+/* renamed to setStatus, but this is left as it has an implementation including semi-immunity.
+var newStatus = function ( skillDef, round )
+{
+	// Probability of applying a status should be part of the skill.
+	// For instance: Pokémon's Toxic has 100% of probability but
+	// Poison Fang only 50%.
+	// var rand = (Math.random() < 0.5) ? -1: 1;
+
+	if ( this.status[ skillDef.id ] - round.currentRound() < skillDef.duration )
+		this.status[ skillDef.id ] = [
+		{
+			skillDef.id, skillDef.duration + rand
+		} ];
 }
+*/
 
-var performAction = function(skillDef){
-	for(var i in skillDef.blockedBy){
-		if(this.status[skillDef.blockedBy[i]].length == 1)
-			return false;
-	}
-	return true;
-}
+/**
+ * Returns whether a skill can be performed by this player or not (due to altered status).
+ * @param  {SkillDefinition} skill Skill to be checked.
+ * @return {boolean}               Whether this character can perform this skill or not.
+ */
+var can_perform_action = function ( skill )
+{
+	return !has_status( skill.blockedBy );
+};
 
-var getPasives = function(){}
+var getPasives = function () {};
 
-var doSkill = function(){}
+var doSkill = function () {};
 
-var clientObject = function(){}
+var clientObject = function () {};
 
 // Add here any instance method you want to make public.
 // Key: public name.
 // Value: function to be called.
 var INSTANCE_METHODS = {
-	"hello": hello,
-	"skillList": skillList,
-	"stats": stats,
-	"alterStat": alterStat,
-	"performAction": performAction,
-	"getPasives": getPasives,
-	"doSkill": doSkill,
-	"clientObject": clientObject
+	hello: hello,
+	skillList: skillList,
+	stats: stats,
+	alterStat: alterStat,
+	getPasives: getPasives,
+	doSkill: doSkill,
+	clientObject: clientObject,
+	// API
+	canPerformAction: can_perform_action,
+	damage: damage,
+	hasStatus: has_status,
+	hasAllStatus: has_all_status,
+	setStatus: set_status,
+	unsetStatus: unset_status
 };
 
 // Constructor.
@@ -96,7 +242,14 @@ module.exports = function ( db_source )
 			else if ( docs.length === 0 ) class_loaded.reject( Constants.ERROR_CLASS_NOT_FOUND );
 			else
 			{
-				character.class = docs[ 0 ];
+				character.class = JSON.parse( JSON.stringify( docs[ 0 ] ) );
+				var stats = {};
+				for ( var i in character.class.stats )
+				{
+					var pair = character.class.stats[ i ];
+					stats[ pair.stat.id ] = pair.value;
+				}
+				character.class.stats = stats;
 				class_loaded.resolve();
 			}
 		} );
@@ -250,20 +403,7 @@ module.exports = function ( db_source )
 		{
 
 			character = JSON.parse( JSON.stringify( character ) ); // Transform character to a standard Javascript object.
-			character.status = {};
 
-			model.Status.find(
-					{
-						
-					} ).exec( function ( err, docs )
-					{
-						if ( err ) _das.reject( err );
-						else
-						{
-							for( var i = 0, i<status.length,i++)
-								character.status[docs[i].id] = [];
-						}
-					} );
 			// Here we will assign methods to the character.
 			for ( var j in INSTANCE_METHODS )
 				character[ j ] = INSTANCE_METHODS[ j ];
