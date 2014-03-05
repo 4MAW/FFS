caa5626cd92683d843f6e08ee45b8d6d98f33c0b
Lluís Ulzurrun de Asanza Sàez <sumolari@gmail.com>
2014-03-05 23:19:26 +0100

    Added more statistics to statistics microsite.

--- public/statistics/stats.js
+++ public/statistics/stats.js
@@ -33,144 +33,168 @@ $( document ).ready( function ()
 	function redraw( animation )
 	{
 		var options = {};
-		if ( !animation )
-		{
-			options.animation = false;
-		}
-		else
-		{
-			options.animation = true;
-		}
 
-		var ctx_skills = $( "#skills" ).get( 0 ).getContext( "2d" );
-		var ctx_skills_damages = $( "#skills_damages" ).get( 0 ).getContext( "2d" );
-		var ctx_skills_damages_per_hit = $( "#skills_damages_per_hit" ).get( 0 ).getContext( "2d" );
-		var ctx_status = $( "#status" ).get( 0 ).getContext( "2d" );
-		var ctx_damages = $( "#damages" ).get( 0 ).getContext( "2d" );
+		if ( !animation )
+			options.animation = false;
+		else
+			options.animation = true;
+
+		var ctx = {
+			skills: $( "#skills" ).get( 0 ).getContext( "2d" ),
+			skills_won: $( "#skills_won" ).get( 0 ).getContext( "2d" ),
+			skills_damages: $( "#skills_damages" ).get( 0 ).getContext( "2d" ),
+			skills_damages_per_hit: $( "#skills_damages_per_hit" ).get( 0 ).getContext( "2d" ),
+			classes_won: $( "#classes_won" ).get( 0 ).getContext( "2d" ),
+			team_won: $( "#team_won" ).get( 0 ).getContext( "2d" ),
+			classes_killed: $( "#classes_killed" ).get( 0 ).getContext( "2d" ),
+			classes_rounds: $( "#classes_rounds" ).get( 0 ).getContext( "2d" ),
+			status: $( "#status" ).get( 0 ).getContext( "2d" ),
+			damages: $( "#damages" ).get( 0 ).getContext( "2d" )
+		};
+
+		var base_dataset = function ()
+		{
+			this.fillColor = "rgba(151,187,205,0.5)";
+			this.strokeColor = "rgba(151,187,205,1)";
+			this.pointColor = "rgba(151,187,205,1)";
+			this.pointStrokeColor = "#fff";
+			this.data = [];
+		};
+
+		var alt_dataset = function ()
+		{
+			this.fillColor = "rgba(220,220,220,0.5)";
+			this.strokeColor = "rgba(220,220,220,1)";
+			this.pointColor = "rgba(220,220,220,1)";
+			this.pointStrokeColor = "#fff";
+			this.data = [];
+		};
 
 		var exclude = [ "skills_used", "status_altered", "status_healed" ];
 
-		var skills_data_src = json.skills_used;
-		var skills_data = {
-			labels: [],
-			datasets: [
-			{
-				fillColor: "rgba(151,187,205,0.5)",
-				strokeColor: "rgba(151,187,205,1)",
-				pointColor: "rgba(151,187,205,1)",
-				pointStrokeColor: "#fff",
-				data: []
-			} ]
-		};
-
-		var i, j, c;
-
-		for ( i in skills_data_src )
+		var data_radar = function ( values, aux, callback )
 		{
-			skills_data.labels.push( i );
-			skills_data.datasets[ 0 ].data.push( skills_data_src[ i ] );
-		}
+			var data = {
+				labels: [],
+				datasets: [ new base_dataset() ]
+			};
 
-		var skills_damage_data_src = json.skills_damage;
-		var skills_damage_data = {
-			labels: [],
-			datasets: [
+			for ( var i in values )
 			{
-				fillColor: "rgba(151,187,205,0.5)",
-				strokeColor: "rgba(151,187,205,1)",
-				pointColor: "rgba(151,187,205,1)",
-				pointStrokeColor: "#fff",
-				data: []
-			} ]
-		};
-
-		for ( i in skills_damage_data_src )
-		{
-			skills_damage_data.labels.push( i );
-			skills_damage_data.datasets[ 0 ].data.push( skills_damage_data_src[ i ] );
-		}
-
-		var skills_damage_per_hit_data = {
-			labels: [],
-			datasets: [
-			{
-				fillColor: "rgba(151,187,205,0.5)",
-				strokeColor: "rgba(151,187,205,1)",
-				pointColor: "rgba(151,187,205,1)",
-				pointStrokeColor: "#fff",
-				data: []
-			} ]
-		};
-
-		for ( i in skills_damage_data_src )
-		{
-			skills_damage_per_hit_data.labels.push( i );
-			skills_damage_per_hit_data.datasets[ 0 ].data.push( skills_damage_data_src[ i ] / skills_data_src[ i ] );
-		}
-
-		var status_src = json.status_altered;
-		var healed_status_src = json.status_healed;
-		var status_indexes = {};
-
-		var status_data = {
-			labels: [],
-			datasets: [
-			{
-				fillColor: "rgba(220,220,220,0.5)",
-				strokeColor: "rgba(220,220,220,1)",
-				pointColor: "rgba(220,220,220,1)",
-				pointStrokeColor: "#fff",
-				data: []
-			},
-			{
-				fillColor: "rgba(151,187,205,0.5)",
-				strokeColor: "rgba(151,187,205,1)",
-				pointColor: "rgba(151,187,205,1)",
-				pointStrokeColor: "#fff",
-				data: []
-			} ]
-		};
-
-		for ( i in status_src )
-		{
-			status_data.labels.push( i );
-			status_indexes[ i ] = status_data.labels.length;
-			status_data.datasets[ 0 ].data.push( status_src[ i ] );
-		}
-
-		c = 0;
-		for ( i in healed_status_src )
-		{
-			j = status_indexes[ i ];
-			if ( j === undefined )
-			{
-				status_data.labels.push( i );
-				j = status_data.labels.length;
+				data.labels.push( i );
+				if ( aux === undefined ||  callback === undefined )
+					data.datasets[ 0 ].data.push( values[ i ] );
+				else
+					data.datasets[ 0 ].data.push( callback( values[ i ], aux[ i ] ) );
 			}
-			if ( status_data.datasets[ 1 ].data[ c ] === undefined )
-				status_data.datasets[ 1 ].data[ c ] = 0;
-			status_data.datasets[ 1 ].data[ j ] = status_src[ i ];
-			c++;
-		}
 
-		//This will get the first returned node in the jQuery collection.
-		var skillsChart = new Chart( ctx_skills ).Radar( skills_data,
+			return data;
+		};
+
+		var data_radar_multiset = function ( a, b )
 		{
-			scaleShowLabels: true
-		} );
-		var skillsDamageChart = new Chart( ctx_skills_damages ).Radar( skills_damage_data,
+			var data = {
+				labels: [],
+				datasets: [ new base_dataset(), new alt_dataset() ]
+			};
+
+			var indexes = {}, i;
+
+			for ( i in a )
+			{
+				data.labels.push( i );
+				data.datasets[ 0 ].data.push( a[ i ] );
+				indexes[ i ] = data.labels.length;
+			}
+
+			var c = 0;
+			for ( i in b )
+			{
+				var j = indexes[ i ];
+
+				if ( j === undefined )
+				{
+					data.labels.push( i );
+					j = data.labels.length;
+				}
+
+				if ( data.datasets[ 1 ].data[ c ] === undefined )
+					data.datasets[ 1 ].data[ c ] = 0;
+				data.datasets[ 1 ].data[ j ] = b[ i ];
+
+				c++;
+			}
+
+			return data;
+		};
+
+		var radar_charts = [];
+
+		radar_charts.push(
 		{
-			scaleShowLabels: true
+			data: data_radar( json.skills_used ),
+			selector: ctx.skills
 		} );
-		var skillsDamagePerHitChart = new Chart( ctx_skills_damages_per_hit ).Radar( skills_damage_per_hit_data,
+
+		radar_charts.push(
 		{
-			scaleShowLabels: true
+			data: data_radar( json.skills_damage ),
+			selector: ctx.skills_damages
 		} );
-		var statusChart = new Chart( ctx_status ).Radar( status_data,
+
+		radar_charts.push(
 		{
-			scaleShowLabels: true
+			data: data_radar( json.skills_won ),
+			selector: ctx.skills_won
 		} );
-		var damagesChart = new Chart( ctx_damages ).Doughnut( [
+
+		radar_charts.push(
+		{
+			data: data_radar( json.skills_damage, json.skills_used, function ( a, b )
+			{
+				return a / b;
+			} ),
+			selector: ctx.skills_damages_per_hit
+		} );
+
+		radar_charts.push(
+		{
+			data: data_radar( json.class_won ),
+			selector: ctx.classes_won
+		} );
+
+		radar_charts.push(
+		{
+			data: data_radar( json.class_kills ),
+			selector: ctx.classes_killed
+		} );
+
+		radar_charts.push(
+		{
+			data: data_radar( json.class_rounds ),
+			selector: ctx.classes_rounds
+		} );
+
+		radar_charts.push(
+		{
+			data: data_radar( json.team_won ),
+			selector: ctx.team_won
+		} );
+
+		radar_charts.push(
+		{
+			data: data_radar_multiset( json.status_altered, json.status_healed ),
+			selector: ctx.status
+		} );
+
+		for ( var i in radar_charts )
+			new Chart( radar_charts[ i ].selector ).Radar( radar_charts[ i ].data,
+			{
+				scaleShowLabels: true
+			} );
+
+
+		var damagesChart = new Chart( ctx.damages ).Doughnut( [
 		{
 			value: json.magical_damage_dealed,
 			color: "#F7464A"
@@ -183,7 +207,7 @@ $( document ).ready( function ()
 			scaleShowLabels: true
 		} );
 
-		for ( j in json )
+		for ( var j in json )
 			if ( exclude.indexOf( j ) === -1 )
 				$( "#" + j ).text( json[ j ] );
 
