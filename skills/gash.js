6021ee6e376f01b056ada2a2b0088e50facf3d9a
Lluís Ulzurrun de Asanza Sàez <sumolari@gmail.com>
2014-03-06 18:56:44 +0100

    Added Gash, Guard and Kinetic Wave skills.

---
+++ skills/gash.js
@@ -0,0 +1,23 @@
+var Constants = require( '../vendor/constants.js' );
+
+module.exports = function ()
+{
+	this.type = Constants.PHYSICAL;
+	// Initialization, called when a skill is used.
+	this.init = function ()
+	{
+		this.Round.do( this.damage, this );
+	};
+	this.damage = function ()
+	{
+		this.caller.realDamage( this.cost.amount, this.cost.stat );
+		if ( Constants.LIGHT_ARMOR_TYPES.indexOf( this.target.getArmorType().id ) > -1 )
+			this.target.damage( 2000, this );
+		else if ( Constants.MEDIUM_ARMOR_TYPES.indexOf( this.target.getArmorType().id ) > -1 )
+			this.target.damage( 1000, this );
+		else
+			this.target.damage( 500, this );
+	};
+	// Array of altered status that prevent this skill to be performed.
+	this.blockedBy = [ Constants.PARALYSIS_STATUS_ID, Constants.BOUND_STATUS_ID ];
+};
---
+++ skills/kineticWave.js
@@ -0,0 +1,33 @@
+// You can safely assume that the following attributes are set:
+//
+// - id:          ID of this skill.
+// - name:        Name of this skill.
+// - target:      Target of this skill (if there is only one target). Character.
+// - targets:     Array of targets of this skill. [Character].
+// - caller:      Character performing using this skill.
+// - Round:       Round API.
+// - accuracy:    Accuracy of the skill (0...1).
+// - multiTarget: Whether this skill is a multi target skill or not.
+
+var Constants = require( '../vendor/constants.js' );
+
+module.exports = function ()
+{
+	this.type = Constants.PHYSICAL;
+	// Initialization, called when a skill is used.
+	this.init = function ()
+	{
+		this.Round.do( this.damage, this );
+	};
+	this.damage = function ()
+	{
+		this.caller.realDamage( this.cost.amount, this.cost.stat );
+		// WTF? Daño en función de la fuerza? Es intencionado?
+		// En cualquier caso, si esto se salta la defensa la skill no debería de
+		// tener tipo de daño, ya que el tipo es para elegir el algoritmo, y si
+		// no hay algoritmo...
+		this.target.damage( this.caller.getStat( Constants.STR_STAT_ID ) * 0.6, Constants.ACTUALHP_STAT_ID );
+	};
+	// Array of altered status that prevent this skill to be performed.
+	this.blockedBy = [ Constants.PARALYSIS_STATUS_ID, Constants.BOUND_STATUS_ID ];
+};
