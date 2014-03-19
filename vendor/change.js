b8ee6ecce0b0c15cc5b357acf407b1dc861299e3
Lluís Ulzurrun de Asanza Sàez <sumolari@gmail.com>
2014-03-19 19:19:17 +0100

    Documented Change class.

--- vendor/change.js
+++ vendor/change.js
@@ -1,7 +1,14 @@
 /**
- * Returns a new Change.
+ * A Change is a representation of a modification of the environtment,
+ * specifically a change in a Character's attribute. That attribute can be
+ * a statistic, an altered status, its class or a different lifecycle change.
+ *
+ * @class  Change
+ * @constructor
+ *
  * @param  {Character} target Character that suffers the change.
- * @param  {string}    key    Element that is changing: "stat" | "status" | "class".
+ * @param  {string}    key    Element that is changing:
+ *                            "stat" | "status" | "class" | "lifecycle".
  * @param  {string}    value  Instance of the element that is changing: stat to change or status to change.
  *                            For example, for a stat this could be "00000001", for a status
  *                            if could be "poison" and for a class it is always null.
@@ -10,12 +17,44 @@
  *                            For a class it's the ID of the new class.
  * @return {Change}           Change representing given parameters.
  */
-module.exports = function ( target, key, value, change )
-{
+module.exports = function ( target, key, value, change ) {
+	/**
+	 * Character affected by this change.
+	 * @property character
+	 * @type {Character}
+	 */
 	this.character = target;
+	/**
+	 * Modification performed by this change.
+	 * Values: "+", "-", "+N", "-N", "className", "appear" or "disappear"
+	 * @property change
+	 * @type {string}
+	 */
 	this.change = change;
+	/**
+	 * Attribute being modified by this change.
+	 * @property item
+	 * @type {AttributeChanged}
+	 */
+	/**
+	 * Attribute being modified by a change.
+	 * @class AttributeChanged
+	 */
 	this.item = {
+		/**
+		 * Type of attribute being modified by this change.
+		 * Possible values: "stat" | "status" | "class" | "lifecycle"
+		 * @property key
+		 * @type {string}
+		 */
 		key: key,
+		/**
+		 * Specific attribute being modified by this change.
+		 * For instance, ID of stat being altered.
+		 * When not applicable (changing class) it is null.
+		 * @property value
+		 * @type {string}
+		 */
 		value: value
 	};
 };
